e4d73782573b0eb8af41eebfa35780091c4c3459
Stanislav-Cherak <sirStanislav27@gmail.com>
2021-11-03 23:49:45 +0300

    weather ended

---
+++ Weather/src/js/constants.js
@@ -0,0 +1,5 @@
+export const API_KEY='ba84bfbee5907b5ad2b7fa81a316705c';
+export const DOC=document;
+export const FORM=DOC.querySelector('form');
+export const CITY=DOC.getElementById('city');
+export const COUNTRY=DOC.getElementById('country');
